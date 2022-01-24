090c6c3fa8a7b9f0d8efcd3c6ce0cf8d8557565b
trakBan <81049050+trakBan@users.noreply.github.com>
2022-01-24 12:13:58 +0100

    Update setup.py
    fixed typo, requests now added

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
    name='spongebob-cli',
    version='1.1',
-   install_requires=["termcolor", "BeautifulSoup4", "prettytable", "youtube-dl", "halo], 
+   install_requires=["termcolor", "BeautifulSoup4", "prettytable", "youtube-dl", "halo", "requests"], 
    scripts=[
             'spongebob-cli',
-            "func.py"
+            'func.py',
            ]
 )
