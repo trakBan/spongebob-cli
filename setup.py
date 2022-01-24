a33eb042c4eca8a881879e515a7fd395beb7b2de
yaacornus <96870156+yaacornus@users.noreply.github.com>
2022-01-24 18:10:52 +0800

    add halo in python dependecies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='spongebob-cli',
    version='1.1',
-   install_requires=["termcolor", "BeautifulSoup4", "prettytable", "youtube-dl"], 
+   install_requires=["termcolor", "BeautifulSoup4", "prettytable", "youtube-dl", "halo], 
    scripts=[
             'spongebob-cli',
             "func.py"
