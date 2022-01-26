b6812fcfcacbc009db2e45d6fa5b8dcdac115ae5
seth <getchoo@tuta.io>
2022-01-25 19:34:31 -0500

    turn func.py into a module
    change the name of func.py to Spongebobcli.py, and make a module so it
    can be installed easier with package managers

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(
    name='spongebob-cli',
    version='1.1',
-   install_requires=["termcolor", "BeautifulSoup4", "prettytable", "youtube-dl", "halo", "requests"], 
+   install_requires=["termcolor", "BeautifulSoup4", "prettytable", "youtube-dl", "halo", "requests"],
+   py_modules=[
+            'Spongebobcli'
+           ],
    scripts=[
-            'spongebob-cli',
-            'func.py',
+            'spongebob-cli'
            ]
 )
