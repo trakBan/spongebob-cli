d4957b1e70c7d54bf5c9a34aee28604ee4ad7c5e
redone <elkaboussi@pm.me>
2022-06-06 16:43:52 +0100

    sort import according to PEP8

--- func.py
+++ func.py
@@ -1,14 +1,16 @@
-from termcolor import colored
 import sys
 
-def httperror_assess(e):        
+from termcolor import colored
+
+
+def httperror_assess(e):
     if e in [x for x in range(400, 499)]:
         print(colored(f"Client Error {e}. Please try again later."), "red")
-    
+
     elif e in [x for x in range(500, 599)]:
         print(colored(f"Server Error {e}. Please try again later."), "red")
-    
+
     else:
         print(colored(f"An error occured with status number of {e}"), "red")
-    
-    sys.exit()        
+
+    sys.exit()
