41c5959eaf2dcc87907814d66ebecb61236150de
redone <elkaboussi@pm.me>
2022-06-08 13:57:56 +0100

    get requirements from requirements.txt file

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
+from pathlib import Path
+
+requirements = Path("requirements.txt").read_text().splitlines()
 
 setup(
     name="spongebob-cli",
     version="1.1",
-    install_requires=[
-        "termcolor",
-        "BeautifulSoup4",
-        "prettytable",
-        "youtube-dl",
-        "halo",
-        "requests",
-    ],
+    install_requires=requirements,
     scripts=[
         "spongebob-cli",
         "func.py",
-    ],
+    ]
 )
