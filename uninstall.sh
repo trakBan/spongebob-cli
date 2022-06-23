f59eb6153585b9dc640a378fbb36ff744d82d2e2
trakBan <vukasinovicfran@gmail.com>
2022-06-24 00:26:31 +0200

    init

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [[ $EUID -ne 0 ]]; then
+   echo "Uninstall script must be run as root" 
+   exit 1
+fi
+
+# Deletes spongebob-cli from /usr/bin/
+rm /usr/bin/spongebob-cli
+
+ipfetch || echo "spongebob-cli has been sucesfully uninstalled."
